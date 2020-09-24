a91dc3354377c984b2c36ce8f4dd1f8739635ad2
Athar1988 <hakim.athar.88@gmail.com>
2020-09-24 18:32:20 +0100

    video 3 tt terminer est correcte est ca marche

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProduitsComponent} from './produits/produits.component';
+import {NouveauProduitComponent} from './nouveau-produit/nouveau-produit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "produits",
+   component:ProduitsComponent},
+  {path: "ajouteProduit",
+    component:NouveauProduitComponent},
+  {path: "",
+    redirectTo:"/produits",
+    pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
